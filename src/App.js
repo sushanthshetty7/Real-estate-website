cd322318d9fe027fd618f2219bdebb7e363db60d
sushanthshetty7 <sushanthr052@gmail.com>
2024-03-24 21:38:53 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@
 import Navbar from './components/Navbar/Navbar';
 import './index.scss'
 import './layout.scss'
+import Homepage from './routes/homepage/Homepage';
 
 function App() {
   return (
     <div className="Layout">
+      <div className='container1'>
       <Navbar/>
+      </div>
+      <div className='container2'>
+        <Homepage/>
+      </div>
     </div>
   );
 }
---
+++ src/routes/homepage/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './homepage.scss'
+
+function Homepage() {
+  return (
+    <div className='homepage'>
+        <div className="textbox">
+          text
+        </div>
+        <div className="imagebox">
+          <img src="bg.png" alt="" className="image" />
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
